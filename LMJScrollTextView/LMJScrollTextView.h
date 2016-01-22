96e8bc3c5c502396edcadaa0a6b83a473fd07f14
limingjie <limingjie_mail@163.com>
2016-01-22 10:57:28 +0800

    1.0.0

--- LMJScrollTextView/LMJScrollTextView.h
+++ LMJScrollTextView/LMJScrollTextView.h
@@ -4,8 +4,12 @@
 //
 //  Version 1.0.0
 //
-//  Created by MajorLMJ on 14-12-4.
-//  Copyright (c) 2014年 apple. All rights reserved.
+//  Created by MajorLi on 14-12-4.
+//  Copyright (c) 2014年 iOS开发者公会. All rights reserved.
+//
+//  iOS开发者公会-技术1群 QQ群号：87440292
+//  iOS开发者公会-技术2群 QQ群号：232702419
+//  iOS开发者公会-议事区  QQ群号：413102158
 //
 
 #import <UIKit/UIKit.h>
